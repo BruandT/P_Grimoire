e269a16de11b6e0d0406a2a23e8eb2397591cf8a
Thomas Bruand <thomas.bruand@gmail.com>
2023-07-06 06:57:10 +0200

    Ajout du middleware sharp aux routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const sharp = require('../middleware/sharp-config')
 const bookCtrl = require('../controllers/book');
 
-router.post('/', auth, multer, bookCtrl.createBook);
+router.post('/', auth, multer, sharp, bookCtrl.createBook);
 router.get('/', bookCtrl.getAllBook);
 router.get('/bestrating', bookCtrl.getTopRatedBook)
 router.get('/:id', bookCtrl.getBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 router.post('/:id/rating', auth, bookCtrl.setRatingBook)
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, sharp, bookCtrl.modifyBook);
 
 module.exports = router;
